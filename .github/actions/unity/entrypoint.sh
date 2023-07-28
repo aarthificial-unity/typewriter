5a8ece9a3c05c8b901401544cf6a4818437c4486
aarthificial <aarthificial@gmail.com>
2023-07-28 17:10:12 +0200

    build: generate Unity solution

---
+++ .github/actions/unity/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "::: Unity License Activation :::"
+/opt/unity/Editor/Unity -logFile - -batchmode -nographics -quit -manualLicenseFile /github/workspace/license.ulf
+
+echo "::: Unity Solution Generation :::"
+cd .project
+/opt/unity/Editor/Unity -logFile - -batchmode -nographics -quit -executeMethod Solution.Sync
+
+echo "::: DotNet Installation :::"
+cd ..
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get update
+apt-get install -y dotnet-sdk-7.0
+dotnet tool update -g docfx
+
+echo "::: Documentation Generation :::"
+~/.dotnet/tools/docfx .docfx/docfx.json
---
+++ .project/Assets/Editor/Solution.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+using Packages.Rider.Editor.ProjectGeneration;
+
+public static class Solution {
+  public static void Sync() {
+    AssetDatabase.Refresh();
+    new ProjectGeneration().Sync();
+  }
+}
